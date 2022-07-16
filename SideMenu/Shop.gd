0e92d15a1f9cb023f04d5bc8be385d52222173a9
Alexandra <alexandra95brito@gmail.com>
2022-07-16 10:26:29 +0100

    Added Shop List

---
+++ SideMenu/Shop.gd
@@ -0,0 +1,33 @@
+extends MarginContainer
+
+var shopItems = [
+	{
+		name = "Workshop",
+		price = [3,0,0,1],
+		imgPath = "res://assets/Buildings/castle.png"
+	},
+	 {
+		name = "Wood Harvester",
+		price = [3,1,0,2],
+		imgPath = "res://assets/Buildings/trees.png"
+	},
+	{
+		name = "Stone Harvester",
+		price = [3,1,0,2],
+		imgPath = "res://assets/Buildings/stones.png"
+	},
+	{
+		name = "Gem Harvester",
+		price = [3,1,0,2],
+		imgPath = "res://assets/Buildings/gems.png"
+	},
+	 {
+		name = "Upgrade tools",
+		price = [3,1,0,2],
+		imgPath = "res://assets/Buildings/trees.png"
+	},
+]
+
+func _ready():
+	for key in shopItems:
+		
