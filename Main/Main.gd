e278ae0b3dd016adde2c1f6c17373dec1814e0e6
Alexandra <alexandra95brito@gmail.com>
2022-07-16 17:17:06 +0100

    Added resource update

--- Main/Main.gd
+++ Main/Main.gd
@@ -1,25 +1,25 @@
 extends Node2D
 
 var gameRound = 0
-
-var wood = 0
-var stone = 0
-var gem = 0
-var mana = 0
+export var resources = [13,11,1,5]
 
 var toolsLevel = 1
 
 func _ready(): 
-	pass
-
-##This Logic will probaily go to SideMenu
+	SetResources()
+	
 var random = RandomNumberGenerator.new()
 func RollDice():
 	var diceValue = random.randi_range(1, 6)
 	print(diceValue)
 	return diceValue
 
-##This Logic will probaily go to SideMenu
 func goToNextRound():
 	gameRound+=1
-	mana += RollDice()
+	resources[3] += RollDice()
+
+func SetResources():
+	get_node("SideMenu/MarginContainer/Resources/Wood/Quantity").text = str(resources[0])
+	get_node("SideMenu/MarginContainer/Resources/Stone/Quantity").text = str(resources[1])
+	get_node("SideMenu/MarginContainer/Resources/Gem/Quantity").text = str(resources[2])
+	get_node("SideMenu/MarginContainer/Resources/Mana/Quantity").text = str(resources[3])
--- SideMenu/SideMenu.gd
+++ SideMenu/SideMenu.gd
@@ -1,7 +1,15 @@
 extends MarginContainer
 
+
 func _ready():
 	pass
+#	SetResources()
+#
+#func SetResources():
+#	get_node("MarginContainer/Resources/Wood/Quantity").text = str(resources[0])
+#	get_node("MarginContainer/Resources/Stone/Quantity").text = str(resources[1])
+#	get_node("MarginContainer/Resources/Gem/Quantity").text = str(resources[2])
+#	get_node("MarginContainer/Resources/Mana/Quantity").text = str(resources[3])
 
 func _on_NextRoundBtn_pressed():
 	var MainScene = get_parent()
